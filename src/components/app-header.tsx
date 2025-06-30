7ca1496394e6d0954793a353a0ddb37ce9e601e9
oguzyucel1 <oguzyucell.oy@gmail.com>
2025-07-01 01:46:08 +0300

    header ui added

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -3,7 +3,7 @@ import ToggleTheme from "./toggle-theme";
 
 export default function AppHeader() {
     return <div className="container">
-        <div className="border border-neutral-200 rounded-[1.25rem] bg-neutral-0 py-3 px-4 dark:border-none dark:bg-neutral-800 flex items-center justify-between">
+        <div className="border border-neutral-200 rounded-[1.25rem] bg-neutral-0 py-3 px-4 dark:border-none dark:bg-neutral-800 flex items-center justify-between max-sm:py-2 max-sm:px-3">
             <AppLogo />
             <ToggleTheme />
         </div>
