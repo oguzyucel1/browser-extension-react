4fd5b7f0eccef122da52aa39dafb72d8414ed294
oguzyucel1 <oguzyucell.oy@gmail.com>
2025-07-01 01:28:54 +0300

    inital

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import  { Header, Main } from "./app-layout"
+import AppHeader from "./components/app-header"
+
+function App() {
+
+  return (
+    <>
+      <Header>
+          <AppHeader />
+      </Header>
+
+      <Main>
+        
+      </Main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/app-header.tsx
@@ -0,0 +1,11 @@
+import AppLogo from "./app-logo";
+import ToggleTheme from "./toggle-theme";
+
+export default function AppHeader() {
+    return <div className="container">
+        <div className="border border-neutral-200 rounded-[1.25rem] bg-neutral-0 py-3 px-4 dark:border-none dark:bg-neutral-800 flex items-center justify-between">
+            <AppLogo />
+            <ToggleTheme />
+        </div>
+    </div>
+}
